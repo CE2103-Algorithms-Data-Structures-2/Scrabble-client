03038719d9ded3582686c17cc37b07403da060bd
dcamachog1501 <dcamachog1501@gmail.com>
2019-03-08 23:49:40 -0600

    Commit inicial

---
+++ cmake-build-debug/CMakeFiles/Proyecto_1_Client.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Proyecto_1_Client.dir/main.cpp.o"
+  "Proyecto_1_Client.pdb"
+  "Proyecto_1_Client"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Proyecto_1_Client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
