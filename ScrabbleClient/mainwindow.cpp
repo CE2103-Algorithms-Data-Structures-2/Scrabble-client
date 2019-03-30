1339de0baf3baf481010d46f3b51e02ddcfa9217
David <david.azoherra@gmail.com>
2019-03-29 23:56:17 -0600

    mejora funcionalidad ventanas

--- ScrabbleClient/mainwindow.cpp
+++ ScrabbleClient/mainwindow.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <string>
 
+#define LOG(x) std::cout << x << std::endl;
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -27,7 +29,33 @@ void MainWindow::on_pushButton_10_clicked()
 
 void MainWindow::on_pushButton_14_clicked()
 {
-    std::string nomLobby = ui->lineEdit_2->text().toStdString();
-    std::string nomJugador = ui->lineEdit_3->text().toStdString();
-    std::string numJugadores = ui->comboBox->currentText().toStdString();
+    if (!ui->lineEdit_2->text().toStdString().compare("") && !ui->lineEdit_3->text().toStdString().compare("")) {
+        std::string nomLobby = ui->lineEdit_2->text().toStdString();
+        std::string nomJugador = ui->lineEdit_3->text().toStdString();
+        std::string numJugadores = ui->comboBox->currentText().toStdString();
+
+        LOG(nomLobby);
+        LOG(nomJugador);
+        LOG(numJugadores);
+    }
+
+
+
+}
+
+void MainWindow::on_lineEdit_returnPressed()
+{
+    std::string codigo = ui->lineEdit->text().toStdString();
+
+    LOG(codigo)
+}
+
+void MainWindow::on_pushButton_11_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
+}
+
+void MainWindow::on_pushButton_13_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(0);
 }
--- ScrabbleClient/mainwindow.h
+++ ScrabbleClient/mainwindow.h
@@ -22,6 +22,12 @@ private slots:
 
     void on_pushButton_14_clicked();
 
+    void on_lineEdit_returnPressed();
+
+    void on_pushButton_11_clicked();
+
+    void on_pushButton_13_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
